e1996ebacf25f91e06d61563038ed9fedeb93175
Tasnif Emran <tasnifemran@gmail.com>
2025-11-24 11:13:10 +0600

    Mongodb added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
 
-import notesRoutes from "./routes/notesRoutes.js"
-
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5001
+
+connectDB();
+
 app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+    console.log("Server started on PORT: " + PORT);
 });
